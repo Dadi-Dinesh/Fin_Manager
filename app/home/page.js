33978b7be275ccf59c57caefcc25fbdb3931b8a6
Your Name <youremail@yourdomain.com>
2025-05-07 15:47:00 +0530

    login added

---
+++ app/home/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">FinManager</div>
+            <div className="navbar-profile">profileDiv</div>
+        </nav>
+    )
+}
